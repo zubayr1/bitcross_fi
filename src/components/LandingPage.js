e004ee7d9795b1db24fa6c92d54160dbe8bd3a67
Zubayr Khalid <zkhalid@audeering.com>
2024-07-06 11:20:25 +0200

    Add initial animaiton

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,15 +1,39 @@
-import React from 'react'
-import Header from './Header'
-import Landing from './Landing'
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import Landing from './Landing';
+import Loading from './Loading';
 
 function LandingPage() {
-  return (
-    <div style={{backgroundColor:'black'}}>
-        <Header/>
+  const [loading, setLoading] = useState(true);
 
-        <Landing/>
+  useEffect(() => {
+    // Check if the GIF has already been shown in the current session
+    const hasSeenGif = sessionStorage.getItem('hasSeenGif');
+    if (hasSeenGif) {
+      setLoading(false);
+    } else {
+      setLoading(true);
+    }
+  }, []);
+
+  const handleGifEnd = () => {
+    // Mark the GIF as shown in sessionStorage
+    sessionStorage.setItem('hasSeenGif', 'true');
+    setLoading(false);
+  };
+
+  return (
+    <div style={{ backgroundColor: 'black' }}>
+      {loading ? (
+        <Loading onGifEnd={handleGifEnd} />
+      ) : (
+        <>
+          <Header />
+          <Landing />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
---
+++ src/components/Loading.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import xLogoGif from '../assets/x_logo.gif'; // Ensure the path is correct
+
+function Loading({ onGifEnd }) {
+  useEffect(() => {
+    // Set a timeout equal to the duration of the GIF to simulate the end of the GIF
+    const timeout = setTimeout(() => {
+      onGifEnd();
+    }, 5200); // Replace 3000 with the duration of your GIF in milliseconds
+
+    return () => clearTimeout(timeout);
+  }, [onGifEnd]);
+
+  return (
+    <div style={{ backgroundColor: 'black', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <img src={xLogoGif} alt="Loading" style={{ width: '80%', height: 'auto' }} />
+    </div>
+  );
+}
+
+export default Loading;
